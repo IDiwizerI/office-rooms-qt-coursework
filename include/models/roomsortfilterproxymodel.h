dad1fa523e22127cbc1a48abde157f40f7c42293
IDiwizerI <63365287+IDiwizerI@users.noreply.github.com>
2026-05-28 01:47:11 +0300

    Update roomsortfilterproxymodel.h

---
+++ include/models/roomsortfilterproxymodel.h
@@ -0,0 +1,10 @@
+#pragma once
+
+#include <QSortFilterProxyModel>
+
+/**
+ * @brief Proxy model for sorting and filtering room records.
+ */
+class RoomSortFilterProxyModel : public QSortFilterProxyModel
+{
+};
