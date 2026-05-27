158a6034925fd8005fbd492f2106891252970dd5
IDiwizerI <63365287+IDiwizerI@users.noreply.github.com>
2026-05-28 01:46:49 +0300

    Update roomtablemodel.h

---
+++ include/models/roomtablemodel.h
@@ -0,0 +1,10 @@
+#pragma once
+
+#include <QAbstractTableModel>
+
+/**
+ * @brief Table model for displaying rooms stored in a container.
+ */
+class RoomTableModel : public QAbstractTableModel
+{
+};
